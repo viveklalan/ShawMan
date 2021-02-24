57db16cbe8f25cb1bc933263568500e9c7be5243
ms-vivek <vivek.kantilal@markelytics.com>
2021-02-24 21:49:45 +0530

    Initial Commit

---
+++ ShawManApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShawManApp
+//
+//  Created by Jai Mataji on 24/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
