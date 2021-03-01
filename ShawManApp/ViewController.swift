44dad51e8dfe4c73e5de22e525f99f01d61655a2
ms-vivek <vivek.kantilal@markelytics.com>
2021-03-01 23:55:16 +0530

    implemented FormatedAddressResponse and get current location

--- ShawManApp/ViewController.swift
+++ ShawManApp/ViewController.swift
@@ -7,29 +7,59 @@
 
 import UIKit
 import Alamofire
+import CoreLocation
 
-class ViewController: BaseVC {
-
+class ViewController: BaseVC, CLLocationManagerDelegate {
+    
+    var locationManager:CLLocationManager!
+    var locationData: FormatedAddressResponse!
+    
+    var currentLatLong = (latitude: "", longitude: "")
     override func viewDidLoad() {
         super.viewDidLoad()
-        getFormatedAddress()
-        getRestaurantDetailsByFilter()
-        getSingleRestaurantDetails()
-        // Do any additional setup after loading the view.
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled(){
+            locationManager.requestLocation()
+        }
     }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            print("latitude = \(location.coordinate.latitude)")
+            print("longitude = \(location.coordinate.longitude)")
+            currentLatLong = ("\(location.coordinate.latitude)", "\(location.coordinate.longitude)")
 
-
+             
+//            currentLatLong = ("19.1105765", "73.0174654")
+            getFormatedAddress()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error \(error)")
+    }
+    
+    
     // MARK: API Call
     func getFormatedAddress(){
         self.showProgressIndicator()
         let parameters: [String:String] = [
-            "StrLocLatitude":"19.1105765",
-            "StrLocLongitude":"73.0174654"
+            "StrLocLatitude":currentLatLong.latitude,
+            "StrLocLongitude":currentLatLong.longitude
         ]
         APIMananger.getFormatedAddress(parameters: parameters).responseDecodable(of: FormatedAddressResponse.self) { response in
             self.hideProgressIndicator()
             if let data = response.value{
-
+                self.locationData = data
+                self.getRestaurantDetailsByFilter()
+                if let message = data.Message{
+                    self.showAlert(title: "Error", message: message)
+                }
             }else{
                 self.showAlert(title: "Error", message: "Something went wrong, please try later")
             }
@@ -39,16 +69,16 @@ class ViewController: BaseVC {
     func getRestaurantDetailsByFilter(){
         self.showProgressIndicator()
         let parameters: [String:String] = [
-            "StrLocLatitude":"19.1105765",
-            "StrLocLongitude":"73.0174654",
-            "StrLocCityName":"19.1105765",
+            "StrLocLatitude":currentLatLong.latitude,
+            "StrLocLongitude":currentLatLong.longitude,
+            "StrLocCityName":self.locationData.CityName,
             "IntLocOrderby":"1",
             "IntLocNoOfRecords":"0"
         ]
         APIMananger.getRestaurantDetailsByFilter(parameters: parameters).responseDecodable(of: RestaurantDetailsByFilterResponse.self) { response in
             self.hideProgressIndicator()
             if let data = response.value{
-
+                
             }else{
                 self.showAlert(title: "Error", message: "Something went wrong, please try later")
             }
@@ -58,14 +88,14 @@ class ViewController: BaseVC {
     func getSingleRestaurantDetails(){
         self.showProgressIndicator()
         let parameters: [String:String] = [
-            "StrLocLatitude":"19.114644",
-            "StrLocLongitude":"73.0229018",
+            "StrLocLatitude":currentLatLong.latitude,
+            "StrLocLongitude":currentLatLong.longitude,
             "IntLocRestaurantId":"639161"
         ]
         APIMananger.getSingleRestaurantDetails(parameters: parameters).responseDecodable(of: SingleRestaurantDetailResponse.self) { response in
             self.hideProgressIndicator()
             if let data = response.value{
-
+                
             }else{
                 self.showAlert(title: "Error", message: "Something went wrong, please try later")
             }
